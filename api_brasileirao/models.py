f8db3abd11ce7f97b1cccdd86614e9291f161931
Edilson Alves <email@edilsonalves.com>
2019-12-17 13:04:20 -0300

    Create Team model (api_brasileirao)

--- api_brasileirao/models.py
+++ api_brasileirao/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your models here.
+
+class Team(models.Model):
+    code = models.CharField(primary_key=True, max_length=3)
+    name = models.CharField(max_length=50)
+    points = models.IntegerField(default=0)
+    matches = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    draws = models.IntegerField(default=0)
+    defeats = models.IntegerField(default=0)
+    gf = models.IntegerField(default=0)
+    ga = models.IntegerField(default=0)
+    gd = models.IntegerField(default=0)
+    percentage = models.IntegerField(default=0)
+
+    permission_class = (IsAuthenticatedOrReadOnly,)
+
+    def __str__(self):
+        return self.name
